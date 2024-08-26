102e6896eb1d11cedb39114fc063ec5722fa9853
santoshpatil96 <santosh.patil@fugoservices.com>
2024-08-26 11:22:45 +0530

    ui changes changes

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -18,7 +18,7 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="w-9 px-0 mt-1 border shadow">
+        <Button variant="ghost" className="w-9 px-0 mt-1">
           <SunIcon className="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all hover:fill-black dark:text-white dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1rem] w-[1rem] rotate-90 scale-0 hover:fill-black dark:text-white transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
